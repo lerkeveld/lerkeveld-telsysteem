3a19f98fb4eed33ea5f12bf46beb4aa205d034c8
Michiel V <verbauwhede.michiel@hotmail.com>
2020-02-21 23:21:19 +0100

    implemented tab autocompletion of first suggestion in AutoCompleteTextfield

--- src/CountSystem/supportElements/AutocompleteTextField.java
+++ src/CountSystem/supportElements/AutocompleteTextField.java
@@ -1,10 +1,11 @@
 package CountSystem.supportElements;
 
 import javafx.geometry.Side;
-import javafx.scene.control.ContextMenu;
 import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Font;
 
 import java.util.Collection;
@@ -20,7 +21,7 @@ public class AutocompleteTextField extends TextField {
     // a function that returns the autocomplete suggestions based on the input of the field
     private Function<String, Collection<String>> getCompletions;
 
-    private ContextMenu completions = new ContextMenu();
+    private GoldfishContextMenu completions = new GoldfishContextMenu();
 
     private double menuItemTextSize = 12;
 
@@ -31,7 +32,7 @@ public class AutocompleteTextField extends TextField {
         // the outermost lambda expression gets converted into a changeListener
         textProperty().addListener((observableValue, s, t1) -> { // s is old value, t1 is new value
             if (!getText().equals("")) {
-                completions.getItems().clear(); // clear the current suggestions
+                completions.getItems().clear();
                 // create the new suggestions with the autocomplete functionality on click/enter
                 getCompletions.apply(t1).stream().map(st -> new CustomMenuItem(new Label(st), true)).forEach(item -> {
                     item.setOnAction(
@@ -46,12 +47,25 @@ public class AutocompleteTextField extends TextField {
                 // do not show if the size is too large
                 if (completions.getItems().size() < MAXSIZE)
                     completions.show(AutocompleteTextField.this, Side.BOTTOM, 0, 0);
-                else completions.hide();
-            } else completions.hide();
+                else {
+                    completions.hide();
+                }
+            } else {
+                completions.hide();
+            }
         });
 
         // do not show suggestions if the text field is not in focus
         focusedProperty().addListener((observableValue, aBoolean, aBoolean2) -> completions.hide());
+
+        // autocomplete the first element of the completions list into the text field when pressing tab
+        addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.TAB && completions.getItems().size() > 0) {
+                setText(((Label) ((CustomMenuItem) completions.getItems().get(0)).getContent()).getText());
+                completions.hide();
+                keyEvent.consume(); // prevent other tab functions like changing focus to other button or textfield
+            }
+        });
     }
 
     // scale the menu items and the text field
---
+++ src/CountSystem/supportElements/GoldfishContextMenu.java
@@ -0,0 +1,16 @@
+package CountSystem.supportElements;
+
+import javafx.scene.control.ContextMenu;
+
+// a contextMenu that forgets all its items when hiding
+public class GoldfishContextMenu extends ContextMenu {
+    public GoldfishContextMenu(){
+        super();
+    }
+
+    @Override
+    public void hide(){
+        super.hide();
+        getItems().clear();
+    }
+}
