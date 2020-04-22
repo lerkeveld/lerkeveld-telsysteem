b159a5be55af9a140f161888f88dff6ce288cf10
Michiel Verbauwhede <verbauwhede.michiel@hotmail.com>
2020-04-22 20:57:44 +0200

    added icon and title to application

--- src/main/java/lerkeveld/telsysteem/App.java
+++ src/main/java/lerkeveld/telsysteem/App.java
@@ -2,6 +2,7 @@ package lerkeveld.telsysteem;
 
 import lerkeveld.telsysteem.countSystem.CountSystem;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -16,6 +17,8 @@ public class App extends Application {
         primaryStage.setScene(new CountSystem());
         primaryStage.setWidth(1080);
         primaryStage.setHeight(720);
+        primaryStage.getIcons().add(new Image(this.getClass().getClassLoader().getResourceAsStream("icon.png")));
+        primaryStage.setTitle("Lerkeveld Telsysteem 24-uren loop");
         primaryStage.show();
     }
 }
